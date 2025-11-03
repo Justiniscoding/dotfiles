c5f7148cd0c2bc12d951b99ec1a8a7109c3bd780
Justiniscoding <nanxnan@proton.me>
2025-11-03 11:50:01 -0500

    Update snackspicker colors and make part of lualine transparent

--- nvim/lua/plugins/ayu.lua
+++ nvim/lua/plugins/ayu.lua
@@ -5,11 +5,14 @@ return {
 		priority = 1000,
 		config = function()
 			require("ayu").setup({
-				mirage = false, -- Set to `true` for mirage variant
-				terminal = true, -- true = ayu sets terminal colors
+				mirage = false,
+				terminal = true,
 				overrides = {
 					LineNr = { fg = "#ffb454" },
 					CursorLineNr = { fg = "#51b3ec" },
+					SnacksPickerPathHidden = { fg = "#7fd962" },
+					SnacksPickerPathIgnored = { fg = "#409fff" },
+					SnacksPickerGitStatusUntracked = { fg = "#f26d78" },
 				},
 			})
 			vim.cmd("colorscheme ayu")
